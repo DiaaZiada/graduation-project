d9b27407c52dd84660ce10e140670babea3b26df
DiaaZiada <diaa.elsayedziada@gmail.com>
2020-05-07 07:24:25 +0200

    Update run.py
    update requests methodology

--- run.py
+++ run.py
@@ -1,35 +1,40 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
 from utils import train, predict, video_to_emb
+import json
 
 app = Flask(__name__)
 
 
 
 
-@app.route('/new_student/')
+@app.route('/new_student', methods=['POST'])
 def new_student():
-    video_path = request.args.get("video_path")
-    id = request.args.get("id")
+    video_path = request.json["video_path"]
+    id = request.json["id"]
     video_to_emb(video_path,id)
-    return "done"
+    return 'done'
 
 
-@app.route('/make_section/')
+@app.route('/make_section', methods=['POST'])
 def make_section():
-    ids = request.args.get("ids")
-    group_name = request.args.get("group_name")
+    ids = request.json["ids"]
+    group_name = request.json["group_name"]
+    print(ids)
+    print(group_name)
     train(ids, group_name)
     return "done"
 
-@app.route('/predict/')
+@app.route('/predict', methods=['POST'])
 def url_predict():
-    image = request.args.get("image")
-    group_name = request.args.get("group_name")
+    image = request.json["image"]
+    group_name = request.json["group_name"]
+    print(image, group_name)
     out = predict(image, group_name)
     data = {
         "label":str(out[1][0]),
         "acc":str(out[0])
     }
-    return data
+    print(data)
+    return jsonify(data)
 if __name__ == "__main__":
     app.run(debug=True)
